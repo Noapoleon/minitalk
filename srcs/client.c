dd8f84f924f4ae298bba68e460516985cff80bc5
Noapoleon <noalegrand088@gmail.com>
2023-02-09 22:44:41 +0100

    managed to send message 'ok' but nothing longer yet

--- srcs/client.c
+++ srcs/client.c
@@ -6,41 +6,65 @@
 /*   By: nlegrand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 09:22:01 by nlegrand          #+#    #+#             */
-/*   Updated: 2023/02/09 18:31:44 by nlegrand         ###   ########.fr       */
+/*   Updated: 2023/02/09 22:39:26 by nlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	send_char(unsigned char c, pid_t pid_server)
+{
+	size_t	i;
+	int		ret;
+
+	i = 0;
+	while (i < sizeof(unsigned char) * 8)
+	{
+		ret = 0;
+		if ((c >> i) & 1)
+			ret = kill(pid_server, SIGUSR1);
+		else
+			ret = kill(pid_server, SIGUSR2);
+		if (ret != 0)
+		{
+				perror("[MINITALK] send_char > kill");
+				exit(EXIT_FAILURE);
+		}
+		ft_printf("sent something to server\n");
+		++i;
+		pause();
+	}
+}
+
+void	receive_server_confirmation(int signal, siginfo_t *info, void *other)
+{
+	// dont know if i can make it so that it only resumes when the signal comes from the server
+	// but it shouldn't even matter i think
+	(void)signal;
+	(void)info;
+	(void)other;
+}
+
 int	main(int ac, char **av)
 {
 	pid_t			pid_server;
 	size_t			i;
-	size_t			j;
-	unsigned char	tmp;
+	struct sigaction	action;
 
 	if (ac != 3)
-		return (ft_printf(USAGE_CLIENT, av[0]), 0);  
+		return (ft_printf(USAGE_CLIENT, av[0]), 0);
+	//ft_printf("pid of client is %d\n", getpid());
+	if (sigemptyset(&action.sa_mask) == -1)
+		return (ft_dprintf(2, "[ERROR] Failed to initialize sigaction.\n"), 0);
+	action.sa_sigaction = &receive_server_confirmation;
+	action.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &action, NULL) == -1)
+		return (ft_dprintf(2, "[ERROR] Failed to set SIGUSR1 handle.\n"), 0);
 	pid_server = ft_atoi(av[1]);
 	i = 0;
 	while (av[2][i])
-	{
-		tmp = av[2][i];
-		j = 0;
-		while (j < sizeof(unsigned char) * 8)
-		{
-			if ((tmp >> j) & 1)
-				kill(pid_server, SIGUSR1);
-			else
-				kill(pid_server, SIGUSR2);
-			++j;
-			usleep(50); // debug only
-		}
-		//pause();
-		++i;
-	}
-	//kill(pid_server, SIGUSR1);
-	//usleep(50);
-	//kill(pid_server, SIGUSR2);
+		send_char(av[2][i++], pid_server);
+	send_char('\0', pid_server);
+	ft_printf("Server finished receiving message.\n");
 	return (0);
 }
--- srcs/server.c
+++ srcs/server.c
@@ -6,23 +6,48 @@
 /*   By: nlegrand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 09:22:03 by nlegrand          #+#    #+#             */
-/*   Updated: 2023/02/09 18:36:54 by nlegrand         ###   ########.fr       */
+/*   Updated: 2023/02/09 22:39:54 by nlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-// il bouffe des noa
-void	receive(int signal, siginfo_t *info, void *other)
+void	receive_char(int signal, siginfo_t *info, void *other) // look up what other is for here
 {
-	(void)info;
+	static char				message[2048]; // hard limited not protected yet; can also make this a array of some sort of strut that keeps every client's message status for multiple client sending messages at the same time
+	static unsigned char	character;
+	static int				index;
+	static size_t			shift;
+	char					bit;
+
 	(void)other;
 	if (signal == SIGUSR1)
-		ft_printf("1");
+		bit = 1;
 	else if (signal == SIGUSR2)
-		ft_printf("0");
-	else
+		bit = 0;
+	else // REMOVE LATER, THIS FOR DEBUG
 		ft_printf("WE GOT A PROBLEM HUSTON!\n");
+	//character |= bit << ((sizeof(unsigned char) * 8 - 1) - shift);
+	character  |= bit << shift;
+	++shift;
+	if (shift == 8)
+	{
+		shift = 0;
+		message[index++] = character;
+		if (character == '\0')
+		{
+			ft_putstr_fd(message, 1);
+			index = 0;
+		}
+		character = 0;
+	}
+	//ft_printf("about to send message to client\n");
+	if (kill(info->si_pid, SIGUSR1) == -1)
+	{
+		perror("[MINITALK] kill SIGUSR1");
+		exit(EXIT_FAILURE);
+	}
+	//ft_printf("sent a signal to client of pid %d\n", info->si_pid);
 }
 
 int	main(int ac, char **av)
@@ -34,7 +59,7 @@ int	main(int ac, char **av)
 	ft_printf("Server pid: %d\n", getpid());
 	if (sigemptyset(&action.sa_mask) == -1)
 		return (ft_dprintf(2, "[ERROR] Failed to initialize sigaction.\n"), 0);
-	action.sa_sigaction = &receive;
+	action.sa_sigaction = &receive_char;
 	action.sa_flags = SA_SIGINFO;
 	if (sigaction(SIGUSR1, &action, NULL) == -1)
 		return (ft_dprintf(2, "[ERROR] Failed to set SIGUSR1 handle.\n"), 0);
